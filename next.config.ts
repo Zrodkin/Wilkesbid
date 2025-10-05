1c318c98047fa4140e2fe2a583695a1cecb2cdf9
zrodkin <zalmanrodkin@gmail.com>
2025-10-05 15:55:31 -0400

    fixed error:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    '@react-email/render',
+    'html-to-text',
+    'selderee',
+    '@selderee/plugin-htmlparser2'
+  ],
 };
 
 export default nextConfig;
